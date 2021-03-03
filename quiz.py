efb11de8e6a7fd1b9f1d4de3816bb4809ccb70f4
YairShemer <26732763+YairShemer@users.noreply.github.com>
2021-03-03 19:27:45 +0200

    update

--- quiz.py
+++ quiz.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def hint(hint_title, hint_text, col, score):
+def hint(hint_title, hint_text, col, score, operation):
     if col.checkbox(hint_title):
         col.header(hint_text)
         score = score - 10
@@ -30,15 +30,15 @@ elif selected_person != pre_selected_person:
 
 col1, col2 = st.beta_columns(2)
 
-hint(hint_title='מספר הקילומטרים בין הבית שלך לבית שלי', hint_text='100', col=col1, score=score)
-hint(hint_title='מספר האותיות המשותפות לשמות הפרטיים שלנו:', hint_text='1',col=col1, score=score)
-hint(hint_title='מספר החדר שלך פחות מספר החדר שלי:', hint_text='',col=col1, score=score)
-hint(hint_title='וצאפ ששלחת פעם אלי', hint_text='בפאטיו בצד ימין, מחכים רק לך',col=col2, score=score)
-hint(hint_title='וצאפ ששלחתי פעם אליך', hint_text='ערערתי וקיבלתי אישור לא להיכנס לבידוד (:',col=col2, score=score)
-hint(hint_title='מכר משותף שלנו', hint_text='המנהל שלי לשעבר למד איתך בתואר הראשון',col=col2, score=score)
-# hint(hint_title='', hint_text='',col=col3, score=score)
-# hint(hint_title='', hint_text='',col=col3, score=score)
-# hint(hint_title='', hint_text='',col=col3, score=score)
+hint(hint_title='מספר הקילומטרים בין הבית שלך לבית שלי', hint_text='100', col=col1, score=score, operation=operation)
+hint(hint_title='מספר האותיות המשותפות לשמות הפרטיים שלנו:', hint_text='1',col=col1, score=score, operation=operation)
+hint(hint_title='מספר החדר שלך פחות מספר החדר שלי:', hint_text='',col=col1, score=score, operation = operation)
+hint(hint_title='וצאפ ששלחת פעם אלי', hint_text='בפאטיו בצד ימין, מחכים רק לך',col=col2, score=score, operation = operation)
+hint(hint_title='וצאפ ששלחתי פעם אליך', hint_text='ערערתי וקיבלתי אישור לא להיכנס לבידוד (:',col=col2, score=score, operation = operation)
+hint(hint_title='מכר משותף שלנו', hint_text='המנהל שלי לשעבר למד איתך בתואר הראשון',col=col2, score=score, operation = operation)
+# hint(hint_title='', hint_text='',col=col3, score=score, operation = operation)
+# hint(hint_title='', hint_text='',col=col3, score=score, operation = operation)
+# hint(hint_title='', hint_text='',col=col3, score=score, operation = operation)
 
 
 
